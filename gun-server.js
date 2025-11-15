ab1d3b98009422d4d69c2193475cb53ed8dae148
RITIK VERMA <rv642216@gmail.com>
2025-11-15 13:12:50 +0530

    latest commit
    Refactored Gun server setup and added necessary libraries.

--- gun-server.js
+++ gun-server.js
@@ -1,28 +1,19 @@
-// ------------------------------
-// Simple Dedicated GUN Peer
-// ------------------------------
-
 const Gun = require('gun');
-require('gun/sea');  
-require('gun/lib/auth');  
+require('gun/sea');
+require('gun/lib/radix');
+require('gun/lib/radisk');
+require('gun/lib/store');
+require('gun/lib/rindexed');
+
 const express = require('express');
 const app = express();
 
-// CORS so Netlify frontend can connect
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // allow all for now
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+const port = process.env.PORT || 3000;
+
+app.use(Gun.serve);
+
+const server = app.listen(port, () => {
+  console.log("Gun server running on port " + port);
 });
 
-app.get("/", (req, res) => {
-  res.send("Gun peer running OK");
-});
-
-// Gun config
-const gun = Gun({
-  file: false,
-  web: app.listen(process.env.PORT || 8765, () => {
-    console.log("🚀 GUN peer running on port " + (process.env.PORT || 8765));
-  })
-});
+Gun({ web: server });
